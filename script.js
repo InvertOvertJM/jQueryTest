f8a896a0869f2a67a6b5d1eb5fae2247bfde87f4
Overt$!#& <135649678+InvertOvertJM@users.noreply.github.com>
2025-03-25 12:10:48 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -56,7 +56,7 @@ $(document).ready(function() {
     $("#apply-font").click(function(){
       let fontFam = $("#font-select").val();
       let fontSize = $("#font-size-input").val() + "px";
-      $("#box").css({ "font-size":fontSize});
-      $("#box").css({"font-family": fontFam});
+      $("#box").css( "font-size", fontSize);
+      $("#box").css("font-family",  fontFam);
     });
   });
